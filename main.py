9bd3d9c55d5ef4e6fa14e1bc44de84725ce61fc6
Mateus Braga <teteu040506@gmail.com>
2021-03-01 21:32:36 -0300

    Mudança de planos
    Ao ler a documentação do Discord, vi a existência de uma subclasse 'bot' de discord.Client(). Essa subclasse simplifica ainda mais o processo de processamento de dados e eventos, tornando mais fácil o desenvolvimento do projeto.
    Também foi possível adicionar um comando que retorna uma frase inspiradora do site pensador.com
    Links úteis: https://stackoverflow.com/questions/51234778/what-are-the-differences-between-bot-and-client
    https://discordpy.readthedocs.io/en/latest/ext/commands/api.html#commands

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
-import discord
+from discord.ext import commands
 import os 
+from random import choice
+import requests
+from bs4 import BeautifulSoup
 
+# Bot construído com a subclasse 'bot' do discord.Client
+# Tudo que pode-se fazer com discord.Client, também é possível com bot.
+# https://stackoverflow.com/questions/51234778/what-are-the-differences-between-bot-and-client
+# https://discordpy.readthedocs.io/en/latest/ext/commands/api.html#commands
 
-client = discord.Client()   # Instanciando a conexão com o discord 
+bot = commands.Bot(command_prefix='*')
 
-@client.event  # Decorador para "escutar" eventos do servidor 
-async def on_ready():  # Método que responde o evento. "Callback" 
-    print('Nos estamos logados com {0.user}!'.format(client))  # 0 é uma máscara para client. 
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} está ON!')
+
+# funções que não são do bot, mas usadas por ele. Algo como funções privadas
+def frases():
+    response = requests.get('https://www.pensador.com/frases_de_otimismo/')
+    soup = BeautifulSoup(response.text, 'html.parser')  # Estava escrito hmtl, cuidado!
+    frases = []
+
+    busca = soup.find_all('p', class_='frase fr')
+
+    for i in busca:
+        frases.append(i.text)
+
+    frase = choice(frases)
     
-@client.event 
-async def on_message(message):  # Responde ao evendo "mensagem" 
-    if message.author == client.user:  # Se o autor da mensagem for o bot, não faz nada
-        return
+    return frase
+# -------------------------------------------------------------------------------------------- #
 
-    if message.content.startswith('*salve'):
-        await message.channel.send('SALVE!') 
+# Comando que dá um salve na rapaziada
+@bot.command(name='salve')
+async def salve(ctx):  # ctx é o contexto do bot, que dá informações do servidor e tal
+    await ctx.send('SALVE!')
 
-    # Tô na dúvida desse await, mas se eu não me engano ele espera isso terminar pra poder 
-    # # Executar as outras coisas 
+# Comando que mede o ping (não sei se é o ping com a API)
+@ bot.command(name='ping')
+async def ping(ctx):
+    await ctx.send(f'**Pong!** Meu ping com o server é de `{bot.latency:.2f}ms`')
+    # Aceita Markdown
 
-client.run(os.getenv('TOKEN'))  # Token do BOT no portal do desenvolvedor Discord 
-# Se for usar o repl.it, lembre-se que tudo é público por lá, menos o arquivo .env
+# Comando que mostra uma frase foda KKKKKKKKKKK
+@bot.command(name='frase')
+async def frasefoda(ctx):
+    frase = frases()
+    await ctx.send(f'_{frase}_')
 
-# O nome dos métodos são pré-setados. Se mudarmos o nome deles, não funcionam.
+
+bot.run(os.getenv('DISCORD_TOKEN'))
